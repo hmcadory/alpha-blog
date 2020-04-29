9e62dde6176041401ba1fd79716f51b1593c5b81
Henry McAdory <hmcadory@synchr.com>
2020-04-29 14:51:46 -0400

    Creating articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,15 @@ class ArticlesController < ApplicationController
   def index
     @article = Article.all
   end
+
+  def new
+
+  end
+
+  def create
+    # You must whitelist the fields that you are allowing to be returned - a security feature
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article.save
+    redirect_to @article
+  end
 end
